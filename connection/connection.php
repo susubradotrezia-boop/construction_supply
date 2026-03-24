9d82c72c7a83612d3bbd1d868033efbcaf9bbd17
Trezia <susubradotrezia@gmail.com>
2026-03-25 00:06:40 +0800

    update database

--- connection/connection.php
+++ connection/connection.php
@@ -1,6 +1,15 @@
 <?php
 $host = "caboose.proxy.rlwy.net";
+$port = 8080; // or your Railway port (check dashboard)
 $username = "root";
 $password = "KaqrNecbQPBBPEutuDAeOHirSxcygGWx";
 $dbname = "railway";
+
+// Create connection
+$conn = mysqli_connect($host, $username, $password, $dbname, $port);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
